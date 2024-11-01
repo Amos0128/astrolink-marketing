6f75cd12a2baef4d38311fe48a9a02092d6a3ac7
Herman Liang <herman@koii.network>
2024-11-01 12:43:21 -0300

    testdb update

--- tests/Test-db.js
+++ tests/Test-db.js
@@ -1,6 +1,7 @@
 const { Context } = require('../adapters/context/context');
 async function test(){
     const context = new Context();
-    await context.updateCharInfo();
+    await context.initializeContext();
+    await context.checkUpdates();
 }
 test();
