0c6b11c551ffdbee061df586310194fce649af9d
Soma <soma@koii.network>
2024-11-08 16:03:25 -0400

    remove healdess

--- adapters/twitter/twitter.js
+++ adapters/twitter/twitter.js
@@ -97,7 +97,7 @@ class Twitter extends Adapter {
       this.browser = await stats.puppeteer.launch({
         executablePath: stats.executablePath,
         userDataDir: userDataDir,
-        headless: false,
+        // headless: false,
         userAgent:
           'Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1',
         args: [
