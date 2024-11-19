7cd83d02882b8166b0e883bda68bd382bf82a852
Soma <soma@koii.network>
2024-11-19 13:36:27 -0400

    remove headless

--- adapters/twitter/twitter.js
+++ adapters/twitter/twitter.js
@@ -1925,7 +1925,7 @@ class Twitter extends Adapter {
       );
       let auditBrowser = await stats.puppeteer.launch({
         executablePath: stats.executablePath,
-        headless: false,
+        // headless: false,
         userAgent:
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         args: [
