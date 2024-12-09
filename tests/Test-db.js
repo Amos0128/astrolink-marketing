5229f61052d33fb8b254daaa222723d021a4c231
Soma <soma@koii.network>
2024-12-08 20:42:41 -0400

    remove test file

--- tests/Test-db.js
+++
@@ -1,7 +0,0 @@
-const { Context } = require('../adapters/context/context');
-async function test(){
-    const context = new Context();
-    await context.initializeContext();
-    await context.checkUpdates();
-}
-test();
